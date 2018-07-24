d23c8d8d6c4de72a2bf8dbb1f4a238b646daca69
aosonova <akylai.osonova@zensoft.io>
2018-07-24 13:26:11 +0600

    test

--- src/test/test.js
+++ src/test/test.js
@@ -8,11 +8,14 @@ describe('payload', function () {
 
   it('car quantity with owners older than 20 years', function () {
 
-    let answer;
-
-    /**
-     * you code here
-     */
+    let answer = 0;
+    payload.data.forEach((item) => {
+      item.owners.forEach((element) => {
+        if(element.personalInfo.age > 20 && item.type === 'Car'){
+          answer += 1
+        }
+      });
+    });
 
     assert.equal(answer, 2);
 
@@ -20,23 +23,34 @@ describe('payload', function () {
 
   it('all car colors separated by comma without duplicates', function () {
 
-    let answer;
+    let arr = [];
+    payload.data.forEach((item) => {
+      if(item.type === 'Car') {
+        arr.push(item.attrs.color)
+      }
+    });
 
-    /**
-     * you code here
-     */
+    let unicArr = arr.filter((item, pos) => {
+      return arr.indexOf(item) == pos;
+    });
 
+    let answer = unicArr.join(',');
     assert.equal(answer, 'red,yellow');
 
   });
 
   it('id\'s of all vehicles separated by comma', function () {
 
-    let answer;
+    let arr = [];
+    payload.data.forEach((item) => {
+        arr.push(item.id)
+    });
 
-    /**
-     * you code here
-     */
+    let unicArr = arr.filter((item, pos) => {
+      return arr.indexOf(item) == pos;
+    });
+
+    let answer = unicArr.join(',');
 
     assert.equal(answer, '1,3,6,4,2');
 
@@ -44,11 +58,14 @@ describe('payload', function () {
 
   it('summary price of all items', function () {
 
-    let answer;
+    let arr = [];
+    payload.data.forEach((item) => {
+        arr.push(item.attrs.price)
+    });
 
-    /**
-     * you code here
-     */
+    let answer = arr.reduce((acc, currValue) => {
+      return acc + currValue;
+    }, 0)
 
     assert.equal(answer, 42800);
 
@@ -56,11 +73,19 @@ describe('payload', function () {
 
   it('price of all things john has own', function () {
 
-    let answer;
+    let arr = [];
 
-    /**
-     * you code here
-     */
+    payload.data.forEach((item) => {
+      item.owners.forEach((element) => {
+        if(element.firstName === 'john'){
+          arr.push(item.attrs.price)
+        }
+      });
+    });
+
+    let answer = arr.reduce((acc, currValue) => {
+      return acc + currValue;
+    }, 0)
 
     assert.equal(answer, 25000);
 
@@ -68,12 +93,18 @@ describe('payload', function () {
 
   it('all cities', function () {
 
-    let answer;
+    let arr = [];
+    payload.data.forEach((item) => {
+      item.owners.forEach((element) => {
+        arr.push(element.cities)
+      });
+    });
 
-    /**
-     * you code here
-     */
+    let unicArr = arr.filter((item, pos) => {
+      return arr.indexOf(item) == pos;
+    });
 
+    let answer = unicArr.join(',');
     assert.equal(answer, 'New York,Boston,Columbia,Rapture');
 
   });
